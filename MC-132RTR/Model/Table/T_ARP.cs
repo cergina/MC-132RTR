3f60f2f5fc717a41f0f55c7be2329fb6bf2034f2
maroscerget <maros.cerget@artinsolutions.com>
2019-07-25 18:41:42 +0200

    ARP vol.1

--- MC-132RTR/Model/Table/T_ARP.cs
+++ MC-132RTR/Model/Table/T_ARP.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MC_132RTR.Model.TablePrimitive;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +11,55 @@ namespace MC_132RTR.Model.Table
 {
     public class T_ARP
     {
+        // the only representation of ARP table in code
         private static T_ARP Instance = null;
         public static int TIMEOUT { private set; get; } = 30;
 
+        private IDictionary<IPAddress, TP_ARP> Dict = new Dictionary<IPAddress, TP_ARP>();
+
+
+        public TP_ARP MacToIp(IPAddress key, bool Always)
+        {
+            TP_ARP value;
+            if (Dict.TryGetValue(key, out value))
+            {
+                if (Always || (value != null && value.IsPassable()))
+                {
+                    return value;
+                }
+            }
+            return null;
+        }
+
+        public bool AttemptAddElement(IPAddress key, PhysicalAddress value)
+        {
+            if (!Dict.ContainsKey(key))
+            {
+                Dict.Add(key, new TP_ARP(key, value));
+                return true;
+            }
+
+            // not necessary to detect when to insert in other cases
+            return false;
+        }
+
+        public bool RemoveElemenet(IPAddress Addr)
+        {
+            return Dict.Remove(Addr);
+        }
+
+        public void DisableElement(IPAddress Addr)
+        {
+             TP_ARP Tmp = MacToIp(Addr, true);
+            if (Tmp != null)
+            {
+                Tmp.Deactivate();
+            }
+        }
+
         public void RemoveAllElements()
         {
-
+            Dict.Clear();
         }
 
         public void ChangeTimeout(int Adept)
--- MC-132RTR/Model/TablePrimitive/TP_ARP.cs
+++ MC-132RTR/Model/TablePrimitive/TP_ARP.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using MC_132RTR.Model.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MC_132RTR.Model.TablePrimitive
 {
-    class TP_ARP
+    public class TP_ARP
     {
+        private PhysicalAddress Mac = null;
+        private IPAddress Ip = null;
+        private int Validity;
+
+        public TP_ARP(IPAddress Ip, PhysicalAddress Mac)
+        {
+            this.Mac = Mac;
+            this.Ip = Ip;
+            this.Validity = T_ARP.TIMEOUT;
+        }
+
+        // will be called by some thread
+        private void RegularDecrement()
+        {
+            if (this.Validity > 0)
+                --this.Validity;
+        }
+
+        public void Renew()
+        {
+            this.Validity = T_ARP.TIMEOUT;
+        }
+
+        public void Deactivate()
+        {
+            this.Validity = 0;
+        }
+
+        public bool Matches(IPAddress Adept)
+        {
+            if (Adept.GetAddressBytes().Equals(
+                this.Ip.GetAddressBytes()))
+                return true;
+            else
+                return false;
+        }
+
+        public TP_ARP ReturnIfSuitable(IPAddress Adept) {
+            if (Adept == null)
+                return null;
+
+            if (Matches(Adept))
+                return this;
+            
+            return null;
+        }
+
+        public bool IsPassable()
+        {
+            if (Validity > 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
